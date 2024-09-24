4b45bcbb6be257ce5cddb815fb8cd6ec5d884339
Kummirai <ajaxmilton@hotmail.com>
2024-09-24 21:50:45 +0200

    Update package.json and vite.config.js
    Add dependencies for image optimization in package.json and configure ViteImageOptimizer in vite.config.js.

--- my-first-react-app/vite.config.js
+++ my-first-react-app/vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/Birthdays/'
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      test: /\.(jpe?g|png|gif|tiff|webp|svg|avif)$/i,
+      png: { quality: 80 },
+      jpeg: { quality: 80 },
+      webp: { quality: 80 },
+      avif: { quality: 80 },
+    })
+  ],
+  base: '/Birthdays/',
 });
