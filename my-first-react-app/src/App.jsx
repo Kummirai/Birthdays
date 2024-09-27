dee838f9d8b73e12971df91e890e3a332f4b587e
Kummirai <ajaxmilton@hotmail.com>
2024-09-27 20:00:07 +0200

    Refactor CSS: Add Footer component and update styling

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header.jsx";
 import Button from "./components/Button.jsx";
 import PropTypes from "prop-types";
 import { useState } from "react";
+import Footer from "./components/Footer.jsx";
 
 function Birthday(props) {
   return (
@@ -39,82 +40,85 @@ const MyApp = () => {
   };
 
   return (
-    <div className="card-container">
-      <Header />
-      <div className="months">
-        <Button
-          isSelected={myBirthdays === "JAN"}
-          onSelect={() => selectMonth("JAN")}
-          month={months[0]}
-        />
-        <Button
-          isSelected={myBirthdays === "FEB"}
-          onSelect={() => selectMonth("FEB")}
-          month={months[1]}
-        />
-        <Button
-          isSelected={myBirthdays === "MAR"}
-          onSelect={() => selectMonth("MAR")}
-          month={months[2]}
-        />
-        <Button
-          isSelected={myBirthdays === "APR"}
-          onSelect={() => selectMonth("APR")}
-          month={months[3]}
-        />
-        <Button
-          isSelected={myBirthdays === "MAY"}
-          onSelect={() => selectMonth("MAY")}
-          month={months[4]}
-        />
-        <Button
-          isSelected={myBirthdays === "JUN"}
-          onSelect={() => selectMonth("JUN")}
-          month={months[5]}
-        />
-        <Button
-          isSelected={myBirthdays === "JUL"}
-          onSelect={() => selectMonth("JUL")}
-          month={months[6]}
-        />
-        <Button
-          isSelected={myBirthdays === "AUG"}
-          onSelect={() => selectMonth("AUG")}
-          month={months[7]}
-        />
-        <Button
-          isSelected={myBirthdays === "SEPT"}
-          onSelect={() => selectMonth("SEPT")}
-          month={months[8]}
-        />
-        <Button
-          isSelected={myBirthdays === "OCT"}
-          onSelect={() => selectMonth("OCT")}
-          month={months[9]}
-        />
-        <Button
-          isSelected={myBirthdays === "NOV"}
-          onSelect={() => selectMonth("NOV")}
-          month={months[10]}
-        />
-        <Button
-          isSelected={myBirthdays === "DEC"}
-          onSelect={() => selectMonth("DEC")}
-          month={months[11]}
-        />
-      </div>
+    <>
+      <div className="card-container">
+        <Header />
+        <div className="months">
+          <Button
+            isSelected={myBirthdays === "JAN"}
+            onSelect={() => selectMonth("JAN")}
+            month={months[0]}
+          />
+          <Button
+            isSelected={myBirthdays === "FEB"}
+            onSelect={() => selectMonth("FEB")}
+            month={months[1]}
+          />
+          <Button
+            isSelected={myBirthdays === "MAR"}
+            onSelect={() => selectMonth("MAR")}
+            month={months[2]}
+          />
+          <Button
+            isSelected={myBirthdays === "APR"}
+            onSelect={() => selectMonth("APR")}
+            month={months[3]}
+          />
+          <Button
+            isSelected={myBirthdays === "MAY"}
+            onSelect={() => selectMonth("MAY")}
+            month={months[4]}
+          />
+          <Button
+            isSelected={myBirthdays === "JUN"}
+            onSelect={() => selectMonth("JUN")}
+            month={months[5]}
+          />
+          <Button
+            isSelected={myBirthdays === "JUL"}
+            onSelect={() => selectMonth("JUL")}
+            month={months[6]}
+          />
+          <Button
+            isSelected={myBirthdays === "AUG"}
+            onSelect={() => selectMonth("AUG")}
+            month={months[7]}
+          />
+          <Button
+            isSelected={myBirthdays === "SEPT"}
+            onSelect={() => selectMonth("SEPT")}
+            month={months[8]}
+          />
+          <Button
+            isSelected={myBirthdays === "OCT"}
+            onSelect={() => selectMonth("OCT")}
+            month={months[9]}
+          />
+          <Button
+            isSelected={myBirthdays === "NOV"}
+            onSelect={() => selectMonth("NOV")}
+            month={months[10]}
+          />
+          <Button
+            isSelected={myBirthdays === "DEC"}
+            onSelect={() => selectMonth("DEC")}
+            month={months[11]}
+          />
+        </div>
 
-      {myBirthdays.map((birth, index) => (
-        <Birthday
-          key={index}
-          image={birth.image}
-          name={birth.name}
-          birthday={birth.birthday}
-          job={birth.job}
-          payout={birth.payout}
-        />
-      ))}
-    </div>
+        {myBirthdays.map((birth, index) => (
+          <Birthday
+            key={index}
+            image={birth.image}
+            name={birth.name}
+            birthday={birth.birthday}
+            job={birth.job}
+            payout={birth.payout}
+          />
+        ))}
+      </div>
+      <Footer />
+    </>
   );
 };
 
---
+++ my-first-react-app/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+  const year = new Date().getFullYear();
+
+  return (
+    <footer>
+      <h3>Silver Club</h3>
+      <a href="">
+        <img src="https://e7.pngegg.com/pngimages/884/642/png-clipart-logo-facebook-inc-round-logo-desktop-wallpaper-black-and-white.png" alt="facebook logo" />
+      </a>
+      <p>Join the Club</p>
+      <p>Silver Club &copy; {year}</p>
+    </footer>
+  );
+}
+
+export default Footer;
