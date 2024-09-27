d87882c96089410733f33fe92369fd6f75bb2620
Kummirai <ajaxmilton@hotmail.com>
2024-09-27 20:38:10 +0200

    Refactor CSS: Update Facebook link in Footer component

--- my-first-react-app/src/components/Footer.jsx
+++ my-first-react-app/src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer>
       <h3>Silver Club</h3>
-      <a href="">
+      <a href="https://www.facebook.com/profile.php?id=100091465431429">
         <img src="https://e7.pngegg.com/pngimages/884/642/png-clipart-logo-facebook-inc-round-logo-desktop-wallpaper-black-and-white.png" alt="facebook logo" />
       </a>
       <p>Join the Club</p>
