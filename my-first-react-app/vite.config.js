0b448663403e82875ff5d52eb6483948764774fd
Kummirai <ajaxmilton@hotmail.com>
2024-10-06 16:32:15 +0200

    Refactor vite.config.js: Update image optimization settings and resolve paths

--- my-first-react-app/vite.config.js
+++ my-first-react-app/vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +17,11 @@ export default defineConfig({
       jpeg: { quality: 80 },
       webp: { quality: 80 },
       avif: { quality: 80 },
+      '@': path.resolve(__dirname, './src/assets')
     })
   ],
-  base: '/Birthdays/',
+  resolve: {
+      '@': path.resolve(__dirname, './src/assets'),
+    },
+  base: '/Birthdays/'
 });
